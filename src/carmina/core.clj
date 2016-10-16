d4e09b586c199145784d6e53adf4cbfcfa55343f
Carl Colglazier <carl@carlcolglazier.com>
2016-10-15 20:08:01 -0400

    Implement 1/f noise.

--- src/carmina/core.clj
+++ src/carmina/core.clj
@@ -1,6 +1,40 @@
-(ns carmina.core)
+(ns carmina.core
+   (:require [clojure.math.numeric-tower :as math]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn roll-change [number die-count]
+  "We want to reroll the die each time that the bit the die represents flips.
+   To do this, check if the iterated value divided by 2 times the position
+   changes."
+  (let [n (mod number (math/expt 2 die-count))]
+    (for [x (range 0 die-count)
+          :let
+          [y (if (not=
+                  (quot n (math/expt 2 x))
+                  (quot (mod (dec' n) (math/expt 2 die-count)) (math/expt 2 x)))
+             true
+             false)]] y)))
+
+(defn- roll [x]
+  "Iterate a set of dice to the next count and rerolls where needed."
+  {:count (inc (x :count))
+   :die-count (x :die-count)
+   :sides (x :sides)
+   :die (let [n (roll-change (x :count) (x :die-count))]
+          (for [i (range 0 (x :die-count))]
+            (if (true? (nth n i))
+              (rand-int (x :sides))
+              (nth (x :die) i)
+              )))})
+
+(defn one-f-noise [die-count sides]
+  "Generates a list of integers using 1/f noise."
+  (let [len (math/expt 2 die-count)
+        n (take len
+                (iterate roll { :count 0
+                               :die-count die-count
+                               :die (make-array Integer/TYPE die-count)
+                               :sides sides
+                               } ))]
+    (for [i (range 0 len)]
+      (reduce + (into [] ((nth n i) :die))))))
+
--- test/carmina/core_test.clj
+++ test/carmina/core_test.clj
@@ -2,6 +2,25 @@
   (:require [clojure.test :refer :all]
             [carmina.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest roll-change-test
+  (testing "Do the correct bits return true?"
+    (is (= (into [] '(true true true)) (into [] (roll-change 0 3))))
+    (is (= (into [] '(true false false)) (into [] (roll-change 1 3))))
+    (is (= (into [] '(true true false)) (into [] (roll-change 2 3))))
+    (is (= (into [] '(true false false)) (into [] (roll-change 3 3))))
+    (is (= (into [] '(true true true)) (into [] (roll-change 4 3))))
+    (is (= (into [] '(true false false)) (into [] (roll-change 5 3))))
+    (is (= (into [] '(true true false)) (into [] (roll-change 6 3))))
+    (is (= (into [] '(true false false)) (into [] (roll-change 7 3))))
+    (is (= (into [] '(true true true)) (into [] (roll-change 8 3))))
+    (is (= (into [] '(true false false)) (into [] (roll-change 9 3))))
+    ))
+
+
+(deftest one-f-noise-test
+  (testing "Tests for one-f noise generators."
+    (is (= 8 (count (one-f-noise 3 6))))
+    (is (= 4 (count (one-f-noise 2 6))))
+    ))
+(run-tests 'carmina.core-test)
+
